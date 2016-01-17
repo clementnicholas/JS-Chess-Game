b3a7b51139b726d34cc7dee11be25fd04e868778
Nick Jordan <cnicholasjordan@gmail.com>
2016-01-16 22:48:21 -0800

    refactored game loop for one turn. possible moves are implemented. lots of pseudocode and comments. needs to work on game loop and resetting the listeners after each turn.

--- js/main.js
+++ js/main.js
@@ -1,153 +1,235 @@
 (function() {
-  var rows = ['1', '2', '3', '4', '5', '6', '7', '8'];
   var columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-  $("div.space").on('click', function() {
+  var rows = ['1', '2', '3', '4', '5', '6', '7', '8'];
+  var currentPlayer = 't1';
+  var possibleMoves = [];
 
-    if (!$(this).html()) {
-      return false;
-    }
+  function addHighlight(div) {
+    div.toggleClass('highlight');
+  }
 
-    $('div').removeClass("highlight").removeClass("couldmove");
-    $(this).addClass("highlight");
-    pieceDiv = $(this).children('div');
-    
-    var position, pieceType, team;
+  function clearBackgrounds(div) {
+    div.removeClass('highlight').removeClass('couldmove');
+  }
 
-    var possibleMoves = [];
-    if ( $(this).children ) {
-      position = $(this).attr('id');
-    }
+  function takeTurn(player) {
+    $("div.space").on('click', 'div', function() {
 
-    var classOfPiece = $(this).children('div').attr('class').split(' ');
-    team = classOfPiece[0];
-    pieceType = classOfPiece[1];
+// IF A PIECE IS ALREADY SELECTED, UNHIGHLIGHT AND REMOVE ALL POSSIBLE MOVES
 
-    console.log('team: ' + team + ', piece: ' + pieceType + ', position: ' + position);
+// TODO: REMOVE LISTENERS THAT ENABLE MOVES
 
-    var coordinates = position.split('');
-    
-    switch (pieceType) {
-      case 'pawn':
-        // if team1, move down a space
-        if (team === 't1') {
-          for (var i = 0; i < rows.length; i++) {
-            if (rows[i] === coordinates[1]) {
-              possibleMoves.push(coordinates[0] + rows[i+1]);
+      if ($(this).parent().hasClass('highlight')) {
+        possibleMoves = [];
+        clearBackgrounds($('div'));
 
-        // if in starting position, can move down two spaces
-              if (coordinates[1] === '2' ) {
-                possibleMoves.push(coordinates[0] + rows[i+2]);
-              }            
-            }
-          }
-        } else {
-        // if team2, move up a space 
-          for (var i = 0; i < rows.length; i++) {
-            if (rows[i] === coordinates[1]) {
-              possibleMoves.push(coordinates[0] + rows[i-1]);
+      } else {
 
-        // if in starting position, can move two spaces
-              if (coordinates[1] === '7') {
-                possibleMoves.push(coordinates[0] + rows[i-2]);
-              }
-            }
-          }
-        }
-        console.log(active);
-        possibleMoves.forEach(function(position) {
-          $('#' + position).addClass('couldmove');
-        });
+        var rowIndex, columnIndex;
+        var pieceDiv = $(this);
+        var position = $(this).parent().attr('id'); 
+        var pieceClass = $(this).attr('class').split(' '); 
+        var pieceTeam = pieceClass[0];
+        var pieceType = pieceClass[1];
+        var coordinates = position.split('');
 
-        $('.couldmove').on('click', function() {
-          $(this).html(pieceDiv);
-        });
-        break;
-      case 'knight':
-        console.log('knight clicked');
-        break;
-      case 'castle':
-        console.log('castle clicked');
-        break;
-      case 'bishop':
-        console.log('bishop clicked');
-        break;
-      case 'king':
-        console.log('king clicked');
-        break;
-      case 'queen':
-        console.log('queen clicked');
-        break;
-    }
 
-    if (pieceType === 'knight') {
-      
-
-      var rowIndex = function(arr) {
+// GET PIECE POSITION
         for (var i = 0; i < rows.length; i++) {
-          if (rows[i] === arr[0]) {
-            return i;
+          if (rows[i] === coordinates[1]) {
+            rowIndex = i;
           }
         }
-        return false;          
-      }
 
-      var colIndex = function(arr) {
-        for (var j = 0; j < columns.length; j++) {
-          if (columns[j] === arr[1]) {
-            return j;
+        for (var j = 0; j<columns.length; j++) {
+          if (columns[j] === coordinates[0]) {
+            columnIndex = j;
           }
         }
-      }
-      console.log(coordinates);
-        possibleMoves.push([rows[rowIndex(coordinates) + 2], columns[colIndex(coordinates) + 1]]);
-        possibleMoves.push([rows[rowIndex(coordinates) + 2], columns[colIndex(coordinates) - 1]]);
-        possibleMoves.push([rows[rowIndex(coordinates) - 2], columns[colIndex(coordinates) + 1]]);
-        possibleMoves.push([rows[rowIndex(coordinates) - 2], columns[colIndex(coordinates) - 1]]);
-        possibleMoves.push([rows[rowIndex(coordinates) - 1], columns[colIndex(coordinates) - 2]]);
-        possibleMoves.push([rows[rowIndex(coordinates) - 1], columns[colIndex(coordinates) + 2]]);
-        possibleMoves.push([rows[rowIndex(coordinates) + 1], columns[colIndex(coordinates) - 2]]);
-        possibleMoves.push([rows[rowIndex(coordinates) + 1], columns[colIndex(coordinates) + 2]]);
 
-        var pairs = possibleMoves.map(function(pair){
-          if (pair[0] && pair[1]) {
-            return pair[0] + pair[1];
+// HIGHLIGHT THE PIECE BEING SELECTED TO MOVE
+
+        if (pieceTeam === player) {
+          addHighlight($(this).parent());
+
+// GET POSSIBLE MOVES!!!
+          switch (pieceType) {
+            case 'pawn':
+          // CAN MOVE FORWARD ONE SPACE, TWO IF IN THE STARTING POSITION
+              possibleMoves = [];
+              if (pieceTeam === 't1') {
+                possibleMoves.push(coordinates[0] + rows[rowIndex + 1]);
+          // if in starting position, can move down two spaces
+                if (coordinates[1] === '2' ) {
+                  possibleMoves.push(coordinates[0] + '4');
+                }            
+              } else {
+                possibleMoves.push(coordinates[0] + rows[rowIndex - 1]);
+          // if in starting position, can move down two spaces
+                if (coordinates[1] === '7' ) {
+                  possibleMoves.push(coordinates[0] + '5');
+                }            
+              }
+
+          // IF ENEMY IS DIAGONALLY AHEAD OF PIECE, ADD SPACE TO POSSIBLE MOVES
+
+          // EN PASSANT MOVE
+
+              break;
+
+          case 'knight':
+        // CAN MOVE TWO SPACES IN ONE DIRECTION, ONE IN THE OTHER
+            possibleMoves = [];
+            possibleMoves.push(columns[columnIndex + 2] + rows[rowIndex + 1]);
+            possibleMoves.push(columns[columnIndex - 2] + rows[rowIndex + 1]);
+            possibleMoves.push(columns[columnIndex + 2] + rows[rowIndex - 1]);
+            possibleMoves.push(columns[columnIndex - 2] + rows[rowIndex - 1]);
+            possibleMoves.push(columns[columnIndex + 1] + rows[rowIndex + 2]);
+            possibleMoves.push(columns[columnIndex - 1] + rows[rowIndex + 2]);
+            possibleMoves.push(columns[columnIndex + 1] + rows[rowIndex - 2]);
+            possibleMoves.push(columns[columnIndex - 1] + rows[rowIndex - 2]);
+            break;
+
+          case 'castle':
+        // CAN MOVE UNLIMITED SPACES IN THE SAME COLUMN OR IN THE SAME ROW  
+            possibleMoves = [];
+            possibleMoves.push(columns[columnIndex] + rows[0]);
+            possibleMoves.push(columns[columnIndex] + rows[1]);
+            possibleMoves.push(columns[columnIndex] + rows[2]);
+            possibleMoves.push(columns[columnIndex] + rows[3]);
+            possibleMoves.push(columns[columnIndex] + rows[4]);
+            possibleMoves.push(columns[columnIndex] + rows[5]);
+            possibleMoves.push(columns[columnIndex] + rows[6]);
+            possibleMoves.push(columns[columnIndex] + rows[7]);
+            possibleMoves.push(columns[0] + rows[rowIndex]);
+            possibleMoves.push(columns[1] + rows[rowIndex]);
+            possibleMoves.push(columns[2] + rows[rowIndex]);
+            possibleMoves.push(columns[3] + rows[rowIndex]);
+            possibleMoves.push(columns[4] + rows[rowIndex]);
+            possibleMoves.push(columns[5] + rows[rowIndex]);
+            possibleMoves.push(columns[6] + rows[rowIndex]);
+            possibleMoves.push(columns[7] + rows[rowIndex]);
+            break;
+
+          case 'bishop':
+        // CAN MOVE UNLIMITED SPACES DIAGONALLY. +/+, +/-, -/-, -/+ EACH INDEX
+            possibleMoves.push(columns[columnIndex-columnIndex] + rows[rowIndex-columnIndex]);
+            possibleMoves.push(columns[columnIndex-columnIndex + 1] + rows[rowIndex-columnIndex + 1]);
+            possibleMoves.push(columns[columnIndex-columnIndex + 2] + rows[rowIndex-columnIndex + 2]);
+            possibleMoves.push(columns[columnIndex-columnIndex + 3] + rows[rowIndex-columnIndex + 3]);
+            possibleMoves.push(columns[columnIndex-columnIndex + 4] + rows[rowIndex-columnIndex + 4]);
+            possibleMoves.push(columns[columnIndex-columnIndex + 5] + rows[rowIndex-columnIndex + 5]);
+            possibleMoves.push(columns[columnIndex-columnIndex + 6] + rows[rowIndex-columnIndex + 6]);
+            possibleMoves.push(columns[columnIndex-columnIndex + 7] + rows[rowIndex-columnIndex + 7]);
+            possibleMoves.push(columns[columnIndex-columnIndex] + rows[rowIndex + columnIndex])
+            possibleMoves.push(columns[columnIndex-columnIndex + 1] + rows[rowIndex + columnIndex - 1]);
+            possibleMoves.push(columns[columnIndex-columnIndex + 2] + rows[rowIndex + columnIndex - 2]);
+            possibleMoves.push(columns[columnIndex-columnIndex + 3] + rows[rowIndex + columnIndex - 3]);
+            possibleMoves.push(columns[columnIndex-columnIndex + 4] + rows[rowIndex + columnIndex - 4]);
+            possibleMoves.push(columns[columnIndex-columnIndex + 5] + rows[rowIndex + columnIndex - 5]);
+            possibleMoves.push(columns[columnIndex-columnIndex + 6] + rows[rowIndex + columnIndex - 6]);
+            possibleMoves.push(columns[columnIndex-columnIndex + 7] + rows[rowIndex + columnIndex - 7]);
+            break;
+
+          case 'queen':
+        // CAN MOVE A COMBINATION OF CASTLE AND BISHOP MOVEMENT
+            possibleMoves.push(columns[columnIndex] + rows[0]);
+            possibleMoves.push(columns[columnIndex] + rows[1]);
+            possibleMoves.push(columns[columnIndex] + rows[2]);
+            possibleMoves.push(columns[columnIndex] + rows[3]);
+            possibleMoves.push(columns[columnIndex] + rows[4]);
+            possibleMoves.push(columns[columnIndex] + rows[5]);
+            possibleMoves.push(columns[columnIndex] + rows[6]);
+            possibleMoves.push(columns[columnIndex] + rows[7]);
+            possibleMoves.push(columns[0] + rows[rowIndex]);
+            possibleMoves.push(columns[1] + rows[rowIndex]);
+            possibleMoves.push(columns[2] + rows[rowIndex]);
+            possibleMoves.push(columns[3] + rows[rowIndex]);
+            possibleMoves.push(columns[4] + rows[rowIndex]);
+            possibleMoves.push(columns[5] + rows[rowIndex]);
+            possibleMoves.push(columns[6] + rows[rowIndex]);
+            possibleMoves.push(columns[7] + rows[rowIndex]);
+            possibleMoves.push(columns[columnIndex-columnIndex] + rows[rowIndex-columnIndex]);
+            possibleMoves.push(columns[columnIndex-columnIndex + 1] + rows[rowIndex-columnIndex + 1]);
+            possibleMoves.push(columns[columnIndex-columnIndex + 2] + rows[rowIndex-columnIndex + 2]);
+            possibleMoves.push(columns[columnIndex-columnIndex + 3] + rows[rowIndex-columnIndex + 3]);
+            possibleMoves.push(columns[columnIndex-columnIndex + 4] + rows[rowIndex-columnIndex + 4]);
+            possibleMoves.push(columns[columnIndex-columnIndex + 5] + rows[rowIndex-columnIndex + 5]);
+            possibleMoves.push(columns[columnIndex-columnIndex + 6] + rows[rowIndex-columnIndex + 6]);
+            possibleMoves.push(columns[columnIndex-columnIndex + 7] + rows[rowIndex-columnIndex + 7]);
+            possibleMoves.push(columns[columnIndex-columnIndex] + rows[rowIndex + columnIndex])
+            possibleMoves.push(columns[columnIndex-columnIndex + 1] + rows[rowIndex + columnIndex - 1]);
+            possibleMoves.push(columns[columnIndex-columnIndex + 2] + rows[rowIndex + columnIndex - 2]);
+            possibleMoves.push(columns[columnIndex-columnIndex + 3] + rows[rowIndex + columnIndex - 3]);
+            possibleMoves.push(columns[columnIndex-columnIndex + 4] + rows[rowIndex + columnIndex - 4]);
+            possibleMoves.push(columns[columnIndex-columnIndex + 5] + rows[rowIndex + columnIndex - 5]);
+            possibleMoves.push(columns[columnIndex-columnIndex + 6] + rows[rowIndex + columnIndex - 6]);
+            possibleMoves.push(columns[columnIndex-columnIndex + 7] + rows[rowIndex + columnIndex - 7]);
+            break;
+
+          case 'king':
+        // CAN MOVE ONE SPACE EVERY DIRECTION
+            possibleMoves.push(columns[columnIndex] + rows[rowIndex + 1]);
+            possibleMoves.push(columns[columnIndex] + rows[rowIndex - 1]);
+            possibleMoves.push(columns[columnIndex + 1] + rows[rowIndex + 1]);
+            possibleMoves.push(columns[columnIndex + 1] + rows[rowIndex - 1]);
+            possibleMoves.push(columns[columnIndex - 1] + rows[rowIndex + 1]);
+            possibleMoves.push(columns[columnIndex - 1] + rows[rowIndex - 1]);
+            possibleMoves.push(columns[columnIndex + 1] + rows[rowIndex]);
+            possibleMoves.push(columns[columnIndex - 1] + rows[rowIndex]);
+
+        // TODO: CASTLING
+
+            break;
+        // END SWITCH!
           }
-        });
 
-        console.log(pairs);
-      
+          // FILTER POSSIBLE MOVES. IF A FRIENDLY PIECE OCCUPIES, REMOVE POSSIBLE MOVE
+          // IF A PIECE IS IN THE WAY OF A POSSIBLE MOVE && THE PIECE ISNT A KNIGHT, REMOVE POSSIBLE MOVE
 
-    }
+          possibleMoves.forEach(function(pair) {
 
-  });
-  
+          // ADD LISTENER TO POSSIBLE MOVES
+            $('#'+pair+':empty').addClass('couldmove').on('click', function() {
+          
+          // MOVE IF CLICKED, THEN REMOVE ALL LISTENERS
+              $(this).html(pieceDiv);
 
-  // Set the possible movements for each kind of piece
+              // TODO: IF PIECE IS KILLED, PUT IT IN THE GRAVEYARD OF THAT PLAYER
 
-    // Castle:
-      // Can move to any square in same row:
-      // Can move to any square in same column:
+              // TODO: REMOVE ALL LISTENERS AFTER MOVE
+              
+              // CLEAR HIGHLIGHTS AFTER MOVE
 
-    // Knight:
-      // Can move to any square that is two spaces in a row and 1 space in a column
-      // Can move to any square that is one space in a row and 2 spaces in a column away
+              clearBackgrounds($('div'));
+          
 
-    // Bishop:
-      // Can move to any diagonal square (absolute value of rows away and colums away are equivalent)
+              // CHANGE PLAYER
+              if (currentPlayer === 't1') {
+                currentPlayer = 't2';
+              } else {
+                currentPlayer = 't1';
+              }
 
-    // King:
-      // Can move one square in any direction:
-      // Enemy piece can't be able to attack that square:
+            });
+          });
+        }
+      }
 
-    // Queen:
-      // Can move any amount in a row
-      // Can move any amount in same column
-      // Can move like a Bishop
+// TURN ENDS AFTER A MOVE. PLAYER IS CHANGED. LISTENERS ARE REMOVED.
 
-    // Pawn:
-      // If in starting position, can move 1 or 2 spaces forward
-      // Can move one space forward
+    });
+  }
 
+
+// IF A PIECE GETS CLICKED OFF, REMOVE THE LISTENERS.
+// IF SOMETHING MOVES, REMOVE THE LISTENERS. 
+// IF KILL A PIECE, PUT THAT PIECE IN graveyard
+// IF YOU KILLED THE OPPONENTS KING, END THE GAME, CURRENT PLAYER WINS.
+// END THE TURN.
+// REMOVE ALL LISTENERS
+// CHANGE THE PLAYER.
+// PLAY NEXT TURN.
 
   // Highlight the squares that each piece could move to:
 
@@ -172,7 +254,7 @@
   // if stalemate, end game
 // Switch turns
 
-
+takeTurn(currentPlayer);
 
 
 
